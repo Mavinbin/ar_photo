53d30a89a88a1e42d3408e8047f3dbe6a4363bf0
mavinbin <mavinbin@163.com>
2018-04-03 15:54:36 +0800

    提项目文件

---
+++ js/index.js
@@ -0,0 +1,60 @@
+'use strict'
+
+;
+(function () {
+    var ARPhoto = {}
+
+    // 遍历设备的所有音频和摄像头设备
+    ARPhoto.enumerateDevices = function () {
+        var constraints = {
+            audio: false,
+            video: {
+                width: 960,
+                height: 720
+            }
+        }
+        if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
+            navigator.mediaDevices.enumerateDevices().then(function (devices) {
+                console.log(devices)
+                devices.forEach(function (device) {
+                    if (device.kind === 'videoinput') {
+                        constraints.deviceId = device.deviceId
+                    }
+                })
+
+                ARPhoto.getUserMedia(constraints)
+            }).catch(function (err) {
+                console.error(err)
+            })
+        }
+    }
+
+    // 调用摄像头
+    ARPhoto.getUserMedia = function (constraints) {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+                var oVideo = document.querySelector('#video')
+                oVideo.src = stream
+                oVideo.play()
+                console.log(stream)
+            }).catch(function (err) {
+                console.log(err)
+                if (err.name === 'DevicesNotFoundError') {
+                    console.error('找不到摄像头！')
+                } else {
+                    console.error(err)
+                }
+            })
+        } else {
+            console.error('您的设备不支持摄像头调用！')
+        }
+    }
+
+    // 初始化
+    ARPhoto.init = function () {
+        this.enumerateDevices()
+    }
+
+    ARPhoto.init()
+
+})()
