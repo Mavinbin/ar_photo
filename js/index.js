16174e9bfb0e82903de72a06f1c74d2b0b197e6c
mavinbin <mavinbin@163.com>
2018-04-03 20:17:12 +0800

    测试

--- js/index.js
+++ js/index.js
@@ -14,23 +14,45 @@
         console.log(msg)
     }
 
+    // 检测手机系统
+    ARPhoto.getSystem = function () {
+        var u = navigator.userAgent
+        var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1
+        var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)
+
+        if (isAndroid) {
+            return 'Android'
+        } else {
+            return 'iOS'
+        }
+    }
+
     // 遍历设备的所有音频和摄像头设备
     ARPhoto.enumerateDevices = function () {
         var constraints = {
-            audio: false,
-            video: {}
-        }
+                audio: false,
+                video: {}
+            },
+            videoDevices = []
+
         if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
             navigator.mediaDevices.enumerateDevices().then(function (devices) {
                 devices.forEach(function (device) {
                     if (device.kind === 'videoinput') {
-                        constraints.video.deviceId = device.deviceId
+                        videoDevices.push(device.deviceId)
+
                     }
 
                     // var str = 'device: { deviceId:' + device.deviceId + ', kind:' + device.kind + ', label:' + device.label + '}'
 
                     // ARPhoto.log(str)
                 })
+
+                if (ARPhoto.getSystem() === 'Android') {
+                    constraints.video.deviceId = videoDevices[0]
+                } else {
+                    constraints.video.deviceId = videoDevices[videoDevices.length - 1]
+                }
 
                 ARPhoto.getUserMedia(constraints)
             }).catch(function (err) {
